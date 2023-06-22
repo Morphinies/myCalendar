353245f3a1acc3ed98a79b6036c73f51546a78ee
Cyber Cat <ilya-novyy@mail.ru>
2023-06-22 08:26:00 +0300

    add notes

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 // variables
 let curDate = new Date();
+let choosedDate = new Date();
+const dateTitle = document.getElementById("date-title");
 const dateBtns = document.getElementById("date"); //HEAD DATE BTNS
 const list = document.getElementById("list"); //DAYS LIST
 
+dateTitle.addEventListener("click", () => updateInfo(choosedDate));
+
 // default setting
-addDaysName()
-addDays(curDate);
-addDateBtns(curDate)
+updateInfo(curDate);
 
 // functions
-
-function addDaysName(){
-  ["ПН","ВТ","СР","ЧТ","ПТ","СБ","ВС"].forEach(daysName =>{
-      const item = document.createElement("li");
-      item.className = "btn weeksDay";
-      item.innerHTML = daysName;
-      list.append(item);}
-    )
+function updateInfo(date) {
+  list.innerHTML = "";
+  addDaysName();
+  addDays(date);
+  addDateBtns(date);
+  dateTitle.innerHTML =
+    choosedDate.getDate() +
+    " " +
+    getMonthStr(choosedDate.getMonth(), true) +
+    " " +
+    choosedDate.getFullYear();
 }
 
-function getMonthStr(monthNumb) {
+function addDaysName() {
+  ["ПН", "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"].forEach((daysName) => {
+    const item = document.createElement("li");
+    item.className = "btn weeksDay";
+    item.innerHTML = daysName;
+    list.append(item);
+  });
+}
+
+function getMonthStr(monthNumb, genitive) {
   const months = [
     "январь",
     "февраль",
@@ -34,36 +48,49 @@ function getMonthStr(monthNumb) {
     "ноябрь",
     "декабрь",
   ];
-  return months[monthNumb];
+  const monthsGenetive = [
+    "января",
+    "февраля",
+    "марта",
+    "апреля",
+    "мая",
+    "июня",
+    "июля",
+    "августа",
+    "сентября",
+    "октября",
+    "ноября",
+    "декабря",
+  ];
+  return genitive ? monthsGenetive[monthNumb] : months[monthNumb];
 }
 
-function changeMonth(next) {
-  list.innerHTML = ""
+function prevNextMonth(next) {
+  // list.innerHTML = "";
   if (next) {
     curDate = new Date(curDate.getFullYear(), curDate.getMonth() + 1);
-  }
-  else{
+  } else {
     curDate = new Date(curDate.getFullYear(), curDate.getMonth() - 1);
   }
-  console.log(curDate)
-  addDaysName()
-  addDays(curDate);
-  addDateBtns(curDate)
+  updateInfo(curDate);
 }
 
-function addDateBtns(date){
+function addDateBtns(date) {
   dateBtns.firstElementChild.innerText = getMonthStr(date.getMonth()); //HEAD MONTH
   dateBtns.firstElementChild.nextElementSibling.innerText = date.getFullYear(); // HEAD YEAR
 }
 
 function addDays(date) {
-  const year = date.getFullYear()
-  const month = date.getMonth()
-  const firstWeeksDay = (new Date(year, month)).getDay();
-  if (firstWeeksDay > 0) {
-    for (let k = 1; k < firstWeeksDay; k++) {
-      const dateOld = new Date(date - (firstWeeksDay - k) * 24 * 3600 * 1000);
+  const year = date.getFullYear();
+  const month = date.getMonth();
+  const startDate = new Date(year, month);
+  if (startDate.getDay() > 0) {
+    for (let k = 1; k < startDate.getDay(); k++) {
+      const dateOld = new Date(
+        startDate - (startDate.getDay() - k) * 24 * 3600 * 1000
+      );
       const item = document.createElement("li");
+      item.addEventListener("click", () => setDate(k - startDate.getDay() + 1));
       item.innerHTML = dateOld.getDate();
       item.className = "btn monthsDay";
       item.style.opacity = "30%";
@@ -78,13 +105,11 @@ function addDays(date) {
     const item = document.createElement("li");
     item.className = "btn monthsDay";
     if (+date.getMonth() === +month) {
-      item.innerHTML = date.getDate(); // day of the month
-      const nowadays = new Date();
-      //date.getDate() === curDate.getDate()
+      item.innerHTML = date.getDate();
       if (
-        nowadays.getFullYear() === year &&
-        nowadays.getMonth() === month &&
-        nowadays.getDate() === i
+        choosedDate.getDate() === i &&
+        choosedDate.getMonth() === month &&
+        choosedDate.getFullYear() === year
       ) {
         item.className += " curDay";
       }
@@ -92,22 +117,77 @@ function addDays(date) {
       item.style.opacity = "30%";
       item.innerHTML = date.getDate();
     }
+    item.addEventListener("click", () => setDate(i));
     list.append(item);
   }
 }
 
-function openMonthList(){
-  const monthsList = document.getElementById("months-list")
-  if(monthsList.innerHTML){
-    return monthsList.innerHTML = ""
-  }
-  else{
-    for (let i = 0;i < 12; i++){
-      const li = document.createElement("li");
-      li.innerText = getMonthStr(i);
-      li.className = "btn";
-      monthsList.append(li) 
+function setDate(date) {
+  // list.innerHTML = "";
+  choosedDate = new Date(curDate.getFullYear(), curDate.getMonth(), date);
+  curDate = choosedDate;
+  updateInfo(choosedDate);
+}
+
+function setMonth(month) {
+  // list.innerHTML = "";
+  curDate = new Date(curDate.getFullYear(), month);
+  updateInfo(curDate);
+  openMonthList();
+}
+
+function setYear(year) {
+  // list.innerHTML = "";
+  curDate = new Date(year, curDate.getMonth());
+  updateInfo(curDate);
+  openYearList();
+}
+
+function openMonthList() {
+  const dateList = document.getElementById("date-list");
+  //pressing again
+  if (dateList.innerHTML) {
+    dateList.innerHTML = "";
+    if (dateList.className === "date-list month") {
+      dateList.className = "date-list";
+      return;
     }
   }
-  
+  //adding months
+  dateList.className = "date-list month";
+  for (let i = 0; i < 12; i++) {
+    const li = document.createElement("li");
+    li.innerText = getMonthStr(i);
+    li.className = "btn";
+    if (i === curDate.getMonth()) {
+      li.className = "btn curMonth";
+    }
+    li.addEventListener("click", () => setMonth(i));
+    dateList.append(li);
+  }
+}
+
+function openYearList() {
+  const dateList = document.getElementById("date-list");
+  //pressing again
+  if (dateList.innerHTML) {
+    dateList.innerHTML = "";
+    if (dateList.className === "date-list year") {
+      dateList.className = "date-list";
+      return;
+    }
+  }
+  //adding months
+  dateList.className = "date-list year";
+  for (let i = -25; i < 26; i++) {
+    const li = document.createElement("li");
+    const year = curDate.getFullYear() + i;
+    li.innerText = year;
+    li.className = "btn";
+    if (year === curDate.getFullYear()) {
+      li.className = "btn curYear";
+    }
+    li.addEventListener("click", () => setYear(year));
+    dateList.append(li);
+  }
 }
